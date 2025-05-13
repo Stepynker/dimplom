1a08d76c9d45e9cbbe071de9748ab9283604fb2b
Stepynker <139172832+Stepynker@users.noreply.github.com>
2025-05-13 23:03:06 +0300

    Add files via upload

--- litlinks/server/db/connection.js
+++ litlinks/server/db/connection.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
     host: process.env.DB_HOST || 'localhost', // Используем переменную окружения или localhost для разработки
-    user: process.env.DB_USER || 'root', // Переменная окружения или значение по умолчанию
-    password: process.env.DB_PASSWORD || 'qazwsxedc1234rfvtgb1234', // Переменная окружения или значение по умолчанию
+    user: process.env.DB_USER || 'lit', // Переменная окружения или значение по умолчанию
+    password: process.env.DB_PASSWORD || '123', // Переменная окружения или значение по умолчанию
     database: process.env.DB_NAME || 'litlinks', // Переменная окружения или значение по умолчанию
     port: process.env.DB_PORT || 3306, // Порт (по умолчанию 3306)
 });
