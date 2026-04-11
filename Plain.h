434ce06e9d44e889e07f4ad8538409a608ee1fae
Stepynker <139172832+Stepynker@users.noreply.github.com>
2026-04-11 15:44:19 +0300

    Update Plain.h

--- Plain.h
+++ Plain.h
@@ -1,18 +1,23 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <vector>
+#include "Enemy.h" 
 
 class PlainLocation {
 public:
     bool load();
     void draw(sf::RenderWindow& window);
-
-    std::vector<sf::RectangleShape>& getObstacles() { return obstacles; }
+    const std::vector<sf::RectangleShape>& getObstacles() const { return obstacles; }
     sf::Vector2f getSpawnPos() const { return spawnPos; }
+
+    // Доступ к врагам
+    EnemyManager& getEnemyManager() { return enemyManager; }
 
 private:
     sf::Texture mapTexture;
     sf::Sprite mapSprite;
     std::vector<sf::RectangleShape> obstacles;
     sf::Vector2f spawnPos = { 512, 900 };
+
+    EnemyManager enemyManager;  // Менеджер врагов
 };
